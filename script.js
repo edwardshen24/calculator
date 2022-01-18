91792012d488975f1a28b45dfc9bf196a4bcbf6f
Edward Shen <edwardshen24@gmail.com>
2022-01-17 23:10:21 -0800

    added click listener

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-function add(){
+const display = document.querySelector(".display");
+const keys = document.getElementById("keys");
+const container = document.getElementById("container");
+
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function operate(){
 
 }
 
-function subtract(){
-
-}
-
-function multiply(){
-
-}
-
-function divide(){
-    
-}
+keys.addEventListener('click', e => {
+    if (`${e.target.id}` !== "keys")
+        display.textContent = `${e.target.id}`;
+});
